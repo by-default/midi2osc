62872ff1e27bd9edde1bcff7280e6825b3a8e019
aanper <mail@s3f.ru>
2022-07-09 16:37:34 +0300

    migrate to pythonosc

--- midi2osc.py
+++ midi2osc.py
@@ -6,7 +6,7 @@ import rtmidi
 from rtmidi.midiutil import open_midiinput
 from rtmidi.midiconstants import NOTE_ON, NOTE_OFF, CONTROLLER_CHANGE
 
-import liblo
+from pythonosc import udp_client
 
 import argparse
 
@@ -16,27 +16,25 @@ parser.add_argument("--port", default="9001", help="Sink port")
 parser.add_argument("--midi", default="0", help="midi number")
 args = parser.parse_args()
 
+osc_client = udp_client.SimpleUDPClient(args.host, int(args.port))
+
 def midiin_callback(event, data=None):
     message, deltatime = event
 
     if message[0] & 0xF0 == NOTE_ON or message[0] & 0xF0 == NOTE_OFF:
         status, note, velocity = message
         channel = (status & 0xF) + 1
-        liblo.send(
-            (args.host, args.port),
+        osc_client.send_message(
             f"/midi/{channel}/{'noteon' if message[0] & 0xF0 == NOTE_ON else 'noteoff'}",
-            note,
-            velocity
+            (note, velocity)
         )
 
     if message[0] & 0xF0 == CONTROLLER_CHANGE:
         status, cc, value = message
         channel = (status & 0xF) + 1
-        liblo.send(
-            (args.host, args.port),
+        osc_client.send_message(
             f"/midi/{channel}/cc",
-            cc,
-            value
+            (cc, value)
         )
 
 
